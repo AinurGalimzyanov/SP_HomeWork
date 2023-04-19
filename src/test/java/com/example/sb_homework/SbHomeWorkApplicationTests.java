fd19712f29aa1d4d9afa0c3d96b0e96e13cabbea
Galimzyanov Ainur <a.galimz@mail.ru>
2023-04-19 18:03:26 +0500

    main

---
+++ src/test/java/com/example/sb_homework/SbHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
