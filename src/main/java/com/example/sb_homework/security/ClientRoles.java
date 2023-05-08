dd89f0610372631af73d51529883c559a7471d7b
Galimzyanov Ainur <a.galimz@mail.ru>
2023-05-09 00:51:04 +0500

    исправил

--- src/main/java/com/example/sb_homework/security/ClientRoles.java
+++
@@ -1,8 +0,0 @@
-package com.example.sb_homework.security;
-
-public class ClientRoles {
-    public static final String ROLE_ADMIN = "admin";
-    public static final String ROLE_SUPPORT = "support";
-    public static final String ROLE_USER = "user";
-
-}
