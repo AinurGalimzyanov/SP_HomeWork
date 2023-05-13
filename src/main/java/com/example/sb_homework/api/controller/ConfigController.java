81b278286b574d611a2ba24cc9ca754a5f5c0c97
Galimzyanov Ainur <a.galimz@mail.ru>
2023-05-13 20:54:03 +0500

    переделал

--- src/main/java/com/example/sb_homework/api/controller/ConfigController.java
+++ src/main/java/com/example/sb_homework/api/controller/ConfigController.java
@@ -1,9 +1,7 @@
 package com.example.sb_homework.api.controller;
 
-import com.example.sb_homework.api.config.properties.ProdProperties;
-import lombok.AccessLevel;
+import com.example.sb_homework.api.config.properties.TestProperties;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +9,14 @@ import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ConfigController {
-    final ProdProperties prodProperties;
+    TestProperties testProperties;
     @GetMapping("check/config")
     public HashMap<String, String> getConfigInfo() {
         HashMap<String, String> profileInfo = new HashMap<>();
-        profileInfo.put("env", prodProperties.getEvn());
-        profileInfo.put("app", prodProperties.getApp());
-        profileInfo.put("list", prodProperties.getListValue().toString());
+        profileInfo.put("env", testProperties.getEvn());
+        profileInfo.put("app", testProperties.getApp());
+        profileInfo.put("list", testProperties.getListValue().toString());
         return profileInfo;
     }
 }
