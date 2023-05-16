0b1d9d6d4959feaea722a331b7c23ffaa0c6f369
Galimzyanov Ainur <a.galimz@mail.ru>
2023-05-17 03:37:12 +0500

    brokers

---
+++ src/main/java/com/example/sb_homework/RequestModel.java
@@ -0,0 +1,12 @@
+package com.example.sb_homework;
+
+import lombok.Value;
+
+@Value
+public class RequestModel {
+    String Name;
+
+    Integer Age;
+
+    Boolean DoYouLikeJava;
+}
--- src/main/java/com/example/sb_homework/SbHomeWorkApplication.java
+++ src/main/java/com/example/sb_homework/SbHomeWorkApplication.java
@@ -1,13 +1,25 @@
 package com.example.sb_homework;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
+@EnableBinding(Sink.class)
 @SpringBootApplication
+@Slf4j
 public class SbHomeWorkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SbHomeWorkApplication.class, args);
     }
 
+    @StreamListener(target = Sink.INPUT)
+    public void logfast(RequestModel model) {
+        log.info("Name: " + model.getName());
+        log.info("Age: " + model.getAge());
+        log.info("Do уou like java?: " + model.getDoYouLikeJava());
+    }
 }
