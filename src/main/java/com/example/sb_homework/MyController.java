9cbdef7fa6bed361c46cf1c4708b66a3972ab670
Galimzyanov Ainur <a.galimz@mail.ru>
2023-05-27 15:10:02 +0500

    еще не готово

--- src/main/java/com/example/sb_homework/MyController.java
+++ src/main/java/com/example/sb_homework/MyController.java
@@ -1,10 +1,12 @@
 package com.example.sb_homework;
 
+import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedMetric;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +21,19 @@ public class MyController {
     MetricsService metricsService;
 
     @GetMapping("/kitkat")
+    @Timed(value = "requests.kitkat")
     public String getKitKat() {
         return metricsService.kitKat();
     }
 
     @GetMapping("/twix")
+    @Timed(value = "requests.twix")
     public String getTwix() {
         return metricsService.twix();
     }
 
     @GetMapping("/snickers")
+    @Timed(value = "requests.snickers")
     public String getSnickers() {
         return metricsService.snickers();
     }
